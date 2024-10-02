d02260851056eb95c6bf5a880f4f9fab00855858
g1l4a <128381017+g1l4a@users.noreply.github.com>
2024-10-02 18:00:19 +0300

    Update Program.cs

--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<LibraryContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryContext")));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => {
     // Configure Identity options here
